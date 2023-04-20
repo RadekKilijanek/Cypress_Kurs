4e28b305a923db41ffedca645e7c71f1ea8978ab
Radosław K <radek.kilijanek@gmail.com>
2023-04-20 19:02:18 +0200

    .

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportHeight: 800,
   viewportWidth: 1200,
-
-
+  
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
